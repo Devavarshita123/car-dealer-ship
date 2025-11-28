e9d51ec9e9448859855d4d977a5c11791d6a5028
Devavarshita123 <devavarshita2311@gmail.com>
2025-11-27 20:31:52 -0500

    Updates made

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,36 +1,21 @@
-# server/djangoapp/urls.py
-
+# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 from . import views
 
+
+
 app_name = 'djangoapp'
-
 urlpatterns = [
-    # Path for login page (React frontend)
-    path('djangoapp/login/', TemplateView.as_view(template_name="index.html"), name='login_page'),
+    # # path for registration
+    path('register', views.registration, name='register'),
 
-    # Path for login API
-    path('api/login/', views.login_user, name='login_api'),
+    # path for login
+    path(route='login', view=views.login_user, name='login'),
+     path('logout', views.logout_request, name='logout'),
+    # path for dealer reviews view
 
-    # Path for logout API
-    path('api/logout/', views.logout_user, name='logout_api'),
-
-    # Path for registration API
-    path('api/registration/', views.registration, name='registration_api'),
-
-    # Path to get all dealerships
-    path('api/dealerships/', views.get_dealerships, name='get_dealerships'),
-
-    # Path to get dealer reviews
-    path('api/dealerships/<int:dealer_id>/reviews/', views.get_dealer_reviews, name='get_dealer_reviews'),
-
-    # Path to get dealer details
-    path('api/dealerships/<int:dealer_id>/', views.get_dealer_details, name='get_dealer_details'),
-
-    # Path to add a review
-    path('api/dealerships/<int:dealer_id>/add_review/', views.add_review, name='add_review'),
+    # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
